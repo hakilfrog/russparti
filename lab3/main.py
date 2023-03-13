02f427aa58a13df948819d11661dc1ecbe7ee5e5
hakilfrog <barashiv21@st.ithub.ru>
2023-03-13 17:05:02 +0300

    рефактор и кринж полный

--- lab3/main.py
+++ lab3/main.py
@@ -35,19 +35,21 @@ def gen_users():
 
 
 class Database:
-
     accounts: list[Account] = list()
     groups: list[Group] = list()
 
-    def get_accounts(self):
+    def __init__(self):
         self.accounts = gen_users()
+
+    def get_accounts(self):
         return self.accounts
 
     def add_account(self, account):
         self.accounts.append(account)
 
 
-db = Database()
+
+
 
 class AuthentificationError(Exception):
     pass
@@ -55,6 +57,8 @@ class AuthentificationError(Exception):
 
 class AuthorizationError(Exception):
     pass
+
+
 class CLIUserInput:
     login: str
     password: str
@@ -64,112 +68,77 @@ class CLIUserInput:
         self.login = input("Введите логин")
         self.password = input("Введите пароль")
         self.resource = input("Введите ресурс к которому хотет доступ")
-        return self.login, self.password, self.resource
 
 
-interface = CLIUserInput()
+class CLIUserStub(CLIUserInput):
+    def begin_user_interaction(self):
+        self.login = 'l'
+        self.password = 'p'
+        self.resource = 'r'
+
+
 
 
 class Authentication:
     account: Account
     us_username: str
     us_password: str
-    def __init__(self, ):
-       self.us_username = interface.login
-       self.us_password = interface.password
 
+    def __init__(self):
+        self.us_username = interface.login
+        self.us_password = interface.password
 
     def login_check(self, db):
-        count = 0
-        for account in db.accounts:
-            count += 1
-            if self.us_username == account.username:
-                somechel.password_check(db)
-                break
-            elif count == len(db.accounts):
-                print("Не правильный логин или пароль")
+        if self.us_username == account.username:
+            print(account.username)
+            a.password_check(db)
+        else:
+            print("Не правильный логин или пароль")
 
     def password_check(self, db):
-        count = 0
-        for account in db.accounts:
-            count += 1
-            if self.us_password == account.password:
-                print("Аутефикация прошла успешно")
-                break
-            elif count == len(db.accounts):
-                print("Не правильный логин или пароль1")
 
+        if self.us_password == account.password:
+            print("Аутефикация прошла успешно")
+        else:
+            print("Не правильный логин или пароль1")
 
-
-
-
-
-
-
-
-
-
-
-           # try: interface.login
-           # except AuthentificationError:
-               # print('ERROR #1')
+        # try: interface.login
+        # except AuthentificationError:
+        # print('ERROR #1')
 
     #         try:
     #             self.login_check(db)
     #         except AuthentificationError:
     #              print('ERROR #1')
 
-
-
     def proverka(self, db):
         for account in db.accounts:
-           print(account.username)
-                    #TODO: again login (Anton)
+            print(account.username)
+            # TODO: again login (Anton)
+
+###################################################################
+#interface = CLIUserStub()
+db = Database()
+interfaceU = CLIUserInput()
+
+for account in db.get_accounts():
+    print(account.username, account.password)
+
+
+#interface.begin_user_interaction()
+interfaceU.begin_user_interaction()
+a = Authentication()
+a.login_check(db)
 
 
 
+# a
 
 
-
-
-
-
-
-
-db.get_accounts()
-for account in db.accounts:
-    print(account.username)
-    print(account.password)
-interface.begin_user_interaction()
-somechel = Authentication()
-somechel.login_check(db)
-
-
-
-
-
-
-
-
-
-
-
-
-#a
-
-
-
-
-
-
-#user1 = Account(username='user1', password='user', groups=[groupadmin])
-#db = Database()
-#db.get_accounts()
-#db.add_account(user1)
-#for u in db.accounts:
- #   print(u.username)
-  #  print(u.password)
-
-
-
-
+# user1 = Account(username='user1', password='user', groups=[groupadmin])
+# db = Database()
+# db.get_accounts()
+# db.add_account(user1)
+# for u in db.accounts:
+#   print(u.username)
+#  print(u.password)
