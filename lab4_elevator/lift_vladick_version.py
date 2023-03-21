23204b14bb87a79f1a70080fac84d9ec1530d31e
“hakilfrog” <barashiv21@st.ithub.ru>
2023-03-21 17:13:41 +0300

    кам

--- lab4_elevator/lift_vladick_version.py
+++ lab4_elevator/lift_vladick_version.py
@@ -2,6 +2,8 @@ class ElevatorState:
     def __init__(self, elevator):
         self.elevator = elevator
 
+    def get_state(self):
+        raise NotImplementedError
     # def enter(self, weight):  # писал это сначала, но сейчас не требуется (вроде)
     #     pass
     #
@@ -20,7 +22,7 @@ class EmptyState(ElevatorState):
         self.elevator.current_weight += weight
         self.elevator.state = OccupiedState(self.elevator)
 
-    def exit(self,):
+    def exit(self, ):
         print("Лифт пуст")
 
     def go_up(self):
@@ -29,15 +31,22 @@ class EmptyState(ElevatorState):
     def go_down(self):
         print("Лифт на самом нижнем этаже")
 
+    def get_state(self):
+        return 'Ожидание...'
+
 
 class OccupiedState(ElevatorState):
     def enter(self, weight):
         if self.elevator.current_weight + weight > self.elevator.max_weight:
             print("Перевес!")
+            return False
+
         elif self.elevator.current_weight + weight < self.elevator.min_weight:
             print("Лифт пуст")
+            return False
         else:
             self.elevator.current_weight += weight
+            return True
 
     def exit(self, weight):
         self.elevator.current_weight -= weight
@@ -73,7 +82,7 @@ class Elevator:
 
     def exit(self, weight):
         self.state.exit(weight)
-        print("Текущий вес:" , elevator.current_weight)
+        print("Текущий вес:", elevator.current_weight)
 
     def go_up(self):
         self.state.go_up()
@@ -83,19 +92,25 @@ class Elevator:
         self.state.go_down()
         print("Текущий этаж:", elevator.current_floor)
 
+    def get_state(self):
+        return self.state.get_state()
+
+
 #######################################################
 
 elevator = Elevator(min_weight=10, max_weight=500)
 
 #####################################################
-elevator.enter(0) # вход тела
+print(elevator.get_state())
+elevator.enter(0)  # вход тела
 # print("Текущий вес:", elevator.current_weight) # выводим вес
-print("Current state:", type(elevator.state).__name__)  # почему выводится блять OccupiedState и че с ним делать (наполнение)
-
+# print("Current state:",
+#       type(elevator.state).__name__)  # почему выводится блять OccupiedState и че с ним делать (наполнение) ????
+print(elevator.get_state())
 
 elevator.go_up()
 # print("Текущий этаж:", elevator.current_floor)  # 1 этаж
-
+print(elevator.get_state())
 elevator.go_up()
 # print("Текущий этаж:", elevator.current_floor)  # 2 этаж
 
@@ -105,12 +120,15 @@ elevator.go_up()
 
 elevator.go_up()
 # print("Текущий этаж:", elevator.current_floor)  # а выше не получается да?
-
+print(elevator.get_state())
 
 elevator.go_down()
 # print("Текущий этаж:", elevator.current_floor)  # спускаемся вниз
 
 elevator.exit(0)
 # print("Текущий вес:", elevator.current_weight)  # выход тела
+print(elevator.get_state())
+print("Current state:",
+      type(elevator.state).__name__)  # да как сделать так, чтобы выводилось не имя класса сука сам сука
 
-print("Current state:", type(elevator.state).__name__) # да как сделать так, чтобы выводилось не имя класса сука
+print("Current state:", )
